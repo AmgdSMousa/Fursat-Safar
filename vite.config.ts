6181c3fd838f440894480e81062b416af596655f
AmgdSMousa <34000388+AmgdSMousa@users.noreply.github.com>
2025-10-10 14:21:59 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+// vite.config.ts
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
@@ -5,7 +6,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: '/Fursat-Safar/', // <--- هذا هو السطر الذي سيصلح كل شيء
+      base: '/', // <--- هذا هو الإعداد الصحيح للدومين الخاص
       server: {
         port: 3000,
         host: '0.0.0.0',
